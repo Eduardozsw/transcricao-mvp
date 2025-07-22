f7f412a6ebe2bad2391b066c88ab830c87c0c61a
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-07-21 22:37:39 -0300

    funcionando

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,34 @@
 from fastapi import FastAPI, UploadFile, File
-
+from io import BytesIO
+import wave
+import os
+from vosk import Model, KaldiRecognizer
 app = FastAPI()
 
 @app.get("/")
 def home():
-  return {"mensagem": "API de transcrição"}
+  return {os.path.abspath("modelo")}
 
+
+model = Model("modelo")
+print(os.path.abspath("modelo"))
 @app.post("/envio")
 async def enviar_arquivo(file: UploadFile = File(...)):
   conteudo = await file.read()
-  with open("arquivo_temporario.wav", "wb") as f:
-    f.write(conteudo)
-    return {"nome": file.filename}
+  audio_mem = BytesIO(conteudo)
+
+  with wave.open(audio_mem, "rb") as wf:
+    model= Model("modelo")
+    rec = KaldiRecognizer(model, wf.getframerate())
+
+  resultado = ""
+  while True:
+    data = wf.readframes(4000)
+    if len(data) == 0:
+      break
+    if rec.AcceptWaveform(data):
+      resultado += rec.Result()
+
+  resultado += rec.FinalResult()    
+  return {"nome": file.filename, "Transcrição": resultado}
+
