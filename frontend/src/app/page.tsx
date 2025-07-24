1c86edd746c0b83963af0a50bc221d50262cbcd8
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-07-24 18:41:23 -0300

    estilização

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,11 +3,11 @@ import AudioUploader from "@/components/AudioUploader"
 
 export default function Home() {
   return (
-    <main className="flex flex-col ">
-      <div className="justify-center text-center">
+    <main className="flex flex-col">
+      <div className="justify-center text-center mt-[10%]">
         <h1 className="text-white font-bold text-5xl">Transcritor</h1>
       </div>
-      <div className="justify-center text-center">
+      <div className="justify-center text-center mt-[5%]">
         <AudioUploader />
       </div>
     </main>
