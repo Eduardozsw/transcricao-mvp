6e07cd6440fc5a2a0cc606865c7ff19d5b926a65
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-07-24 00:39:09 -0300

    Design front end

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,15 @@
-import AudioUploader from "@/components/audioUploader"
+import AudioUploader from "@/components/AudioUploader"
+
 
 export default function Home() {
-  return <AudioUploader/>
+  return (
+    <main className="flex flex-col ">
+      <div className="justify-center text-center">
+        <h1 className="text-white font-bold text-5xl">Transcritor</h1>
+      </div>
+      <div className="justify-center text-center">
+        <AudioUploader />
+      </div>
+    </main>
+  )
 }
