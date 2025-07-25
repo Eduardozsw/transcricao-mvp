20125e83b3dc95d6b5cbc939dc9a3cf748957492
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-07-24 23:30:51 -0300

    hero component

---
+++ frontend/src/components/hero.tsx
@@ -0,0 +1,29 @@
+import image from "../../public/image.png"
+import Image from 'next/image'
+
+export default function Hero() {
+  return (
+    <main className="flex flex-col mb-20">
+      <header className="flex justify-around mt-2 border-b-2 border-b-gray-100">
+        <h1 className="text-2xl font-bold">Transcritor</h1>
+        <div className="flex gap-x-4 ">
+          <a href="">Home</a>
+          <a href="">Preços</a>
+          <a href="">Login/Registrar</a>
+        </div>
+      </header>
+      <div className="flex justify-around mt-[5%]">
+        <div className="">
+          <h1 className="text-3xl font-bold max-w-170">Transcreva seus áudios para texto automaticamente.</h1>
+          <div className="flex gap-y-2 flex-col font-medium leading-6.5 my-4">
+            <p className="max-w-150">Converte qualquer tipo de árquivo de áudio para texto, totalmente de graça através do nosso plano free ou experimente nosso plano pago e desfrute de traduções mais fieis ao áudio original.</p>
+            <a href="" className="p-3 w-fit rounded-2xl bg-black text-white">Experimente agora</a>
+          </div>
+        </div>
+        <div>
+          <Image src={image} width={500} height={500} alt="alt" className="rounded-2xl" />
+        </div>
+      </div>
+    </main>
+  )
+}
