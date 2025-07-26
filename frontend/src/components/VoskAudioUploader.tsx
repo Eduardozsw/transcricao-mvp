00fb067a7e3eea2c25fd6d0b66b183c32ccc6846
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-07-26 14:04:12 -0300

    mudei a posição do botão no vosk

--- frontend/src/components/VoskAudioUploader.tsx
+++ frontend/src/components/VoskAudioUploader.tsx
@@ -7,7 +7,7 @@ export default function AudioUploader() {
   const [idioma, setIdioma] = useState("ptbr")
   const [transcricao, setTranscricao] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
-  
+
 
   async function enviar() {
     if (!file) return alert("Por favor, insira um arquivo para continuar.")
@@ -28,25 +28,46 @@ export default function AudioUploader() {
         accept="audio/"
         onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
       />
-      
-        <div className="mt-5">
-          <label className="font-bold  ">Idioma:</label>
-          <select className="bg-transparent active:bg-transparent" value={idioma} onChange={(e) => setIdioma(e.target.value)}>
-            <option className="text-black" value="ptbr">Português</option>
-            <option className="text-black" value="en">Inglês</option>
-            <option className="text-black" value="inen">Inglês sotaque Indiano</option>
-          </select>
-        </div>
 
       <button onClick={enviar} disabled={loading} className="bg-white border-b-4 border-b-gray-300 active:border-b-0 p-2 rounded-2xl mt-5">
         {loading ? "Enviando..." : "Enviar"}
       </button>
 
+      <div className="my-5">
+        <label className="font-bold  ">Idioma:</label>
+        <select className="bg-transparent active:bg-transparent" value={idioma} onChange={(e) => setIdioma(e.target.value)}>
+          <option className="text-black" value="ptbr">Português</option>
+          <option className="text-black" value="en">Inglês</option>
+          <option className="text-black" value="inen">Inglês sotaque Indiano</option>
+        </select>
+      </div>
+
+
       {transcricao && (
-        <div>
-          <h2>Transcrição:</h2>
-          <p>{transcricao}</p>
+        <div className="flex justify-center">
+          <div className="max-w-150">
+            <h2>Transcrição:</h2>
+            <p className="line-clamp-2">{transcricao}</p>
+          </div>
         </div>
+      )}
+      {transcricao && (
+        <button
+          onClick={() => {
+            const blob = new Blob([transcricao], { type: "text/plain" });
+            const url = URL.createObjectURL(blob)
+
+            const a = document.createElement("a");
+            a.href = url
+            a.download = "transcricao.txt"
+            a.click()
+
+            URL.revokeObjectURL(url)
+          }}
+          className="border-b-4 border-b-gray-300 rounded-2xl p-2 active:border-b-0"
+        >
+          Baixar transcrição
+        </button>
       )}
     </div>
   )
