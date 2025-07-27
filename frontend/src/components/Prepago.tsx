12203f58dd2931fdff376e5887a3b52f1de4eb7b
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-07-26 21:12:17 -0300

    prepago

---
+++ frontend/src/components/Prepago.tsx
@@ -0,0 +1,10 @@
+
+export default function Prepago() {
+  return (
+    <main>
+      <div className="bg-gray-100 flex justify-center py-10">
+        <h1 className="text-3xl font-bold">Pacotes pré-pagos</h1>
+      </div>
+    </main>
+  )
+}
