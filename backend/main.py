b05d38437a45c081b8e3fe68240b74f0b901f326
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-08-07 22:56:37 -0300

    ajeitando pra ficar mais seguro

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,14 @@
-import os
-import wave
-import json
-import zipfile
-import whisper
-import requests
-import threading
-from io import BytesIO
-from pydub import AudioSegment
-from vosk import Model, KaldiRecognizer
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, UploadFile, File, Form
+from transcribers.vosk import transcribe_vosk
+from transcribers.whisper import transcribe_whisper
 
 app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "http://localhost:3000/vosk",
-    "http://localhost:3000/whisper",
-    "https://www.transcrevia.com.br",
-    "https://www.transcrevia.com.br/vosk",
-    "https://www.transcrevia.com.br/whisper",
+    "https://www.transcrevia.com.br"
+    
 ]
 
 app.add_middleware(
@@ -30,105 +19,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODELOS_VOSK = {
-    "pt": {
-        "url": "https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip",
-        "pasta": "vosk-model-small-pt-0.3",
-    },
-    "en": {
-        "url": "https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip",
-        "pasta": "vosk-model-small-en-us-0.15",
-    },
-}
-
-locks_por_idioma = {
-    "pt": threading.Lock(),
-    "en": threading.Lock(),
-}
-
-def baixar_modelo(modelo_url, destino, nome_pasta, idioma):
-    lock = locks_por_idioma[idioma]
-    with lock:
-        caminho_extraido = os.path.join(destino, nome_pasta)
-        if os.path.exists(caminho_extraido):
-            return
-        os.makedirs(destino, exist_ok=True)
-        zip_path = os.path.join(destino, "model.zip")
-        print("baixando modelo, aguarde.")
-        with requests.get(modelo_url, stream=True) as r:
-            with open(zip_path, "wb") as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-        with zipfile.ZipFile(zip_path, "r") as zip_ref:
-            zip_ref.extractall(destino)
-        os.remove(zip_path)
-        print("Modelo baixado e extraído")
-
-
-# Converte qualquer formato de áudio para WAV, 16kHz, mono
-async def converter_para_wav(files_bytes: bytes) -> BytesIO:
-    audio = AudioSegment.from_file(BytesIO(files_bytes))
-    audio = audio.set_frame_rate(16000).set_channels(1).set_sample_width(2)
-
-    wav_io = BytesIO()
-    audio.export(wav_io, format="wav")
-    wav_io.seek(0)
-    return wav_io
-
 @app.get("/")
 def read_root():
     return {"message": "Servidor online"}
 
-# Endpoint para transcrição
-@app.post("/vosk")
-async def transcrever_vosk(file: UploadFile = File(...), idioma: str = Form(...)):
+@app.post("/upload")
+async def upload(
+    file: UploadFile = File(...),
+    engine: str = Form(...),
+    idioma: str = Form("Pt")
+):
     conteudo = await file.read()
-    if len(conteudo) > 500 * 1024 * 1024:  # 500 MB
-        return {"error": "Arquivo excede o limite de 500MB"}
-    # Tô baixando os modelos aq
-    modelo_info = MODELOS_VOSK[idioma]
-    url_modelo = modelo_info["url"]
-    nome_pasta = modelo_info["pasta"]
-    destino_modelo = "./model"
-    caminho_modelo = os.path.join(destino_modelo, nome_pasta)
-
-    baixar_modelo(url_modelo, destino_modelo, nome_pasta)
-
-    # Pra salvar temporariamente
-    wav_audio = await converter_para_wav(conteudo)
-    model_path = os.path.abspath(caminho_modelo)
-
-    model = Model(model_path)
-    resultado = []
-
-    with wave.open(wav_audio, "rb") as wf:
-        rec = KaldiRecognizer(model, wf.getframerate())
-
-        while True:
-            data = wf.readframes(1000)
-            if len(data) == 0:
-                break
-            if rec.AcceptWaveform(data):
-                text = json.loads(rec.Result())["text"]
-                resultado.append(text)
-
-        final = json.loads(rec.FinalResult())["text"]
-        resultado.append(final)
-
-    transcricao = " ".join(resultado)
-    return {"nome": file.filename, "transcricao": transcricao}
-
-
-@app.post("/whisper")
-async def transcrever_whisper(file: UploadFile = File(...)):
-    model_whisper = whisper.load_model("tiny")  # modelo whisper
-    conteudo = await file.read()
-    wav_audio = await converter_para_wav(conteudo)
-
-    with open("temp.wav", "wb") as f:
-        f.write(wav_audio.read())
-
-    transcricao = model_whisper.transcribe("temp.wav")
-
-    os.remove("temp.wav")
-    return {"nome": file.filename, "transcricao": transcricao["text"]}
+    if len(conteudo) > 200 * 1024 * 1024:
+        raise HTTPException(status_code=503, detail="Arquivo excedeu o limite de 500MB")
+    if engine == "whisper":
+        texto = await transcribe_whisper(conteudo)
+    elif engine == "vosk":
+        texto = await transcribe_vosk(conteudo, idioma)
+    else:
+        raise HTTPException(status_code=400, detail="Engine inválido")
+    
+    return {"nome": file.filename, "transcrição": texto}
